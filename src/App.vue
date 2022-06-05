3a1b06a7744aeb74b7c0053b0cc3e0a5e3268521
unknown <ojedokunp977@gmail.com>
2022-06-05 21:32:04 +0100

    created card grid and card component

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,28 @@
   <div class="App">
     <h1>Magic Match</h1>
     <button @click="shuffleCards">New Game</button>
+
+    <div class="card-grid">
+      <SingleCard v-for="card in cards" :key="card.id" :card="card" />
+    </div>
   </div>
 </template>
 
 <script>
+import SingleCard from "./components/SingleCard.vue";
+
 export default {
   name: "App",
+  components: { SingleCard },
   data() {
     return {
       cardImages: [
-        { src: "/assets/images/helmet-1.png" },
-        { src: "/assets/images/potion-1.png" },
-        { src: "/assets/images/ring-1.png" },
-        { src: "/assets/images/scroll-1.png" },
-        { src: "/assets/images/shield-1.png" },
-        { src: "/assets/images/sword-1.png" },
+        { src: "helmet-1" },
+        { src: "potion-1" },
+        { src: "ring-1" },
+        { src: "scroll-1" },
+        { src: "shield-1" },
+        { src: "sword-1" },
       ],
       cards: [],
       turns: 0,
@@ -28,45 +35,16 @@ export default {
       const shuffleCards = [...this.cardImages, ...this.cardImages]
         .sort(() => Math.random() - 0.5)
         .map((card) => ({ ...card, id: Math.random() }));
-
       this.cards = shuffleCards;
       this.turns = 0;
     },
+  },
+  mounted() {
+    this.shuffleCards();
   },
 };
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
-
-/* base styles */
-body {
-  font-family: Raleway, sans-serif;
-  margin: 0;
-  font-size: 1.5em;
-  text-align: center;
-  background: #1b1523;
-  color: #fff;
-}
-
-.App {
-  max-width: 860px;
-  margin: 40px auto;
-}
-
-button {
-  background: none;
-  border: 2px solid #fff;
-  padding: 6px 12px;
-  border-radius: 4px;
-  color: #fff;
-  font-weight: bold;
-  cursor: pointer;
-  font-size: 1em;
-}
-
-button:hover {
-  background: #c23866;
-  color: #fff;
-}
+@import "./assets/base.css";
 </style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <div>
+      <img :src="getImgUrl(card.src)" alt="card front" class="front" />
+      <img src="@/assets/images/cover.png" alt="card back" class="back" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card"],
+  methods: {
+    getImgUrl(imgUrl) {
+      const images = require(`@/assets/images/${imgUrl}.png`);
+      return images;
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  position: relative;
+}
+
+.card img {
+  width: 100%;
+  display: block;
+  border: 2px solid #fff;
+  border-radius: 6px;
+}
+</style>
